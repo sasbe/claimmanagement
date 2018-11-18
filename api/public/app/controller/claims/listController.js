6986bae04399bc5e484778a29b944f760758cc5c
sasbe <subedisagar52@gmail.com>
2018-11-18 21:07:25 +0545

    Print dialog added

--- api/public/app/controller/claims/listController.js
+++ api/public/app/controller/claims/listController.js
@@ -1,29 +1,29 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('ClaimListControllers', ['claimServices', 'commonServices'])
-        .controller('claimListController', function($scope, $location, $timeout, Claim, Query, $http) {
+        .controller('claimListController', function ($scope, Claim, Query) {
             var controllerScope = this;
             $scope.sort = {
                 sortingOrder: 'claimno',
                 reverse: false
             };
 
-            $scope.prevPage = function() {
+            $scope.prevPage = function () {
                 if (controllerScope.filters.skip > 0) {
                     controllerScope.filters.decreaseSkip();
                     retrieveClaims(controllerScope.filters.formQueryString());
                 }
             };
 
-            $scope.nextPage = function() {
+            $scope.nextPage = function () {
                 controllerScope.filters.increaseSkip();
                 retrieveClaims(controllerScope.filters.formQueryString());
             };
             controllerScope.claims = [];
             $scope.getAll = false;
-            $scope.$watch("getAll", function() {
+            $scope.$watch("getAll", function () {
                 if ($scope.getAll == true)
                     controllerScope.filters.limit = null;
                 else
@@ -31,25 +31,25 @@
             });
             controllerScope.filters = new Query();
 
-            var retrieveClaims = function(queryFilter) {
+            var retrieveClaims = function (queryFilter) {
                 $scope.$emit("appLoading", true);
-                Claim.getClaims(queryFilter).then(function(response) {
-                        if (response.data.success) {
-                            controllerScope.claims = response.data.claims;
-                        } else {
-                            $scope.$emit("errorReceived", response.data.message);
-                        }
-                        $scope.$emit("appLoading", false);
-                    },
-                    function(response) {
+                Claim.getClaims(queryFilter).then(function (response) {
+                    if (response.data.success) {
+                        controllerScope.claims = response.data.claims;
+                    } else {
+                        $scope.$emit("errorReceived", response.data.message);
+                    }
+                    $scope.$emit("appLoading", false);
+                },
+                    function (response) {
                         $scope.$emit("errorReceived", response.statusText);
                         $scope.$emit("appLoading", false);
                     });
 
             }
-            controllerScope.print = function() {
+            controllerScope.print = function () {
                 $scope.$emit("appLoading", true);
-                Claim.print(controllerScope.claims).then(function(response) {
+                Claim.print(controllerScope.claims).then(function (response) {
                     if (response.data.success) {
                         var ifr = $('<iframe id="secretIFrame" src="" style="display:none; visibility:hidden;"></iframe>');
                         $('body').append(ifr);
@@ -61,21 +61,21 @@
                     }
                 });
             }
-            controllerScope.applyFilter = function() {
+            controllerScope.applyFilter = function () {
                 controllerScope.filters.skip = 0;
                 retrieveClaims(controllerScope.filters.formQueryString());
             }
 
 
-            $(window).resize(function() {
+            $(window).resize(function () {
                 if (window.innerWidth <= 768) {
-                    controllerScope.disableScroll = function() {
+                    controllerScope.disableScroll = function () {
                         $('html').css('overflow', "hidden");
                     }
                     $('.filter>div:first').css('display', 'none');
-                    $(document).on("touchstart", function() {
+                    $(document).on("touchstart", function () {
                         $('.filter>div:first').css('display', 'none');
-                    }).on("touchend", function() {
+                    }).on("touchend", function () {
                         $('.filter>div:first').css({ 'display': '', "top": $(window).height() + $(window).scrollTop() - 28 });
                         var top = 52;
                         if ($(window).scrollTop() > top) {
@@ -88,7 +88,7 @@
                     $('.filter>div:first').css('display', '').css("top", "");
                     $('#filterContainer').css("top", "").css("height", "");
                     $('html').css('overflow', "");
-                    controllerScope.disableScroll = function() {
+                    controllerScope.disableScroll = function () {
                         $('html').css('overflow', "");
                     }
                 }
@@ -98,82 +98,106 @@
             retrieveClaims(controllerScope.filters.formQueryString());
 
         })
-        .controller('todayListController', function($scope, $location, $timeout, Claim, Query, $http) {
+        .controller('todayListController', function ($scope, Claim, Query, $mdDialog, $window, $timeout) {
             var controllerScope = this;
             $scope.sort = {
                 sortingOrder: 'claimno',
                 reverse: false
             };
+            $scope.title = '';
+            $scope.body = '';
 
-            $scope.prevPage = function() {
+            $scope.prevPage = function () {
                 if (controllerScope.filters.skip > 0) {
                     controllerScope.filters.decreaseSkip();
                     retrieveClaims(controllerScope.filters.formQueryString());
                 }
             };
 
-            $scope.nextPage = function() {
+            $scope.nextPage = function () {
                 controllerScope.filters.increaseSkip();
                 retrieveClaims(controllerScope.filters.formQueryString());
             };
             // controllerScope.claims = [];
             $scope.getAll = false;
-            $scope.$watch("getAll", function() {
+            $scope.$watch("getAll", function () {
                 if ($scope.getAll == true)
                     controllerScope.filters.limit = null;
                 else
                     controllerScope.filters.limit = 10;
             });
             controllerScope.filters = new Query();
-            var toDate =  new Date()
+            var toDate = new Date()
             //reset to begining of day
-            toDate.setHours(0,0,0,0); 
+            toDate.setHours(0, 0, 0, 0);
             controllerScope.filters.fromdate = toDate.toISOString();
-            var retrieveClaims = function(queryFilter) {
+            var retrieveClaims = function (queryFilter) {
                 $scope.$emit("appLoading", true);
-                Claim.getClaims(queryFilter).then(function(response) {
-                        if (response.data.success) {
-                            controllerScope.claims = response.data.claims;
-                        } else {
-                            $scope.$emit("errorReceived", response.data.message);
-                        }
-                        $scope.$emit("appLoading", false);
-                    },
-                    function(response) {
+                Claim.getClaims(queryFilter).then(function (response) {
+                    if (response.data.success) {
+                        controllerScope.claims = response.data.claims;
+                    } else {
+                        $scope.$emit("errorReceived", response.data.message);
+                    }
+                    $scope.$emit("appLoading", false);
+                },
+                    function (response) {
                         $scope.$emit("errorReceived", response.statusText);
                         $scope.$emit("appLoading", false);
                     });
 
             }
-            controllerScope.print = function() {
-                $scope.$emit("appLoading", true);
-                Claim.print(controllerScope.claims).then(function(response) {
-                    if (response.data.success) {
-                        var ifr = $('<iframe id="secretIFrame" src="" style="display:none; visibility:hidden;"></iframe>');
-                        $('body').append(ifr);
-                        var iframeURL = response.data.url + "?temp=" + Date.now();
-                        window.open(iframeURL);
-                        $scope.$emit("appLoading", false);
-                    } else {
-                        $scope.$emit("errorReceived", response.data.message);
-                    }
-                });
+            function printDialogController($scope, $mdDialog) {
+                $scope.title = "डिस्चार्ज भौचर सहि गरि पठाइएको छ |";
+                $scope.body = "श्री प्रभू ईन्स्योरेन्स लिमि६े८" +
+                    "केन्द्रीय कार्यालय, तिनकूने का७मा०८ौं ।" +
+                    "" +
+                    "प्रस्तूत विषयमा यस कम्पनीका निम्न लिखित कर्मचारीहरुको तहांबा६ प्राप्त औषधी उपचार ८िस्चार्ज भौचर सही ५ाप गरी यसै पत्र साथ " +
+                    "प७ाइएको व्यहोरा अनूरोध गरिन्५ । " +
+                    "सो  प्राप्त भएप५ि दावी रकम यथासि३्र उपलब्ध गराई दिन’ह’न अन’रोध गरिन्५ ।";
+                $scope.hide = function () {
+                    $mdDialog.hide();
+                };
+
+                $scope.cancel = function () {
+                    $mdDialog.cancel();
+                };
+
+                $scope.answer = function () {
+                    $mdDialog.hide($scope);
+                };
             }
-            controllerScope.applyFilter = function() {
+            controllerScope.print = function (ev) {
+                $mdDialog.show({
+                    controller: printDialogController,
+                    templateUrl: 'app/pages/dialogs/printDialog.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true
+                })
+                    .then(function (dialogScope) {
+                        if(dialogScope){
+                            $scope.title = dialogScope.title;
+                            $scope.body = dialogScope.body;
+                            $timeout($window.print);
+                        }
+                    });
+            }
+            controllerScope.applyFilter = function () {
                 controllerScope.filters.skip = 0;
                 retrieveClaims(controllerScope.filters.formQueryString());
             }
 
 
-            $(window).resize(function() {
+            $(window).resize(function () {
                 if (window.innerWidth <= 768) {
-                    controllerScope.disableScroll = function() {
+                    controllerScope.disableScroll = function () {
                         $('html').css('overflow', "hidden");
                     }
                     $('.filter>div:first').css('display', 'none');
-                    $(document).on("touchstart", function() {
+                    $(document).on("touchstart", function () {
                         $('.filter>div:first').css('display', 'none');
-                    }).on("touchend", function() {
+                    }).on("touchend", function () {
                         $('.filter>div:first').css({ 'display': '', "top": $(window).height() + $(window).scrollTop() - 28 });
                         var top = 52;
                         if ($(window).scrollTop() > top) {
@@ -186,7 +210,7 @@
                     $('.filter>div:first').css('display', '').css("top", "");
                     $('#filterContainer').css("top", "").css("height", "");
                     $('html').css('overflow', "");
-                    controllerScope.disableScroll = function() {
+                    controllerScope.disableScroll = function () {
                         $('html').css('overflow', "");
                     }
                 }
